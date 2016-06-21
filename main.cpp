2c9cba686051cc04c5c5bcdf1e93bc6a15472fae
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2016-06-21 17:11:31 +0900

    use even less vectors

--- main.cpp
+++ main.cpp
@@ -12,16 +12,15 @@ short dump_min3[4];
 
 int16_t getMin(int16x8_t orig0, int16x8_t orig1)
 {
-	int16x4_t _max = vdup_n_s16(0x7fff);      vst1_s16(dump_max, _max);
 	int16x4_t zero = vdup_n_s16(0);           vst1_s16(dump_zero, zero);
 	int16x8_t min0 = vminq_s16(orig0, orig1); vst1q_s16(dump_min0, min0);
 	// min0 <- {1, 2, 4, 8, 16, 32, 64, 128 }
 	int16x4_t min1 = vpmin_s16(vget_low_s16(min0), vget_high_s16(min0));
 	                                          vst1_s16(dump_min1, min1);
 	// min1 <- {1, 4, 16, 64}
-	int16x4_t min2 = vpmin_s16(min1, _max);   vst1_s16(dump_min2, min2);
+	int16x4_t min2 = vpmin_s16(min1, min1);   vst1_s16(dump_min2, min2);
 	// min2 <- {1, 16, 0x7fff, 0x7fff}
-	int16x4_t min3 = vpmin_s16(min2, _max);   vst1_s16(dump_min3, min3);
+	int16x4_t min3 = vpmin_s16(min2, min2);   vst1_s16(dump_min3, min3);
 	// min3 <- {1, 0x7fff, 0x7fff, 0x7fff}
 	return vget_lane_s16(min3, 0);
 }
@@ -52,8 +51,6 @@ int main(int argc, char**argv)
 	std::cout << "min1:"; dump4(dump_min1); std::cout << std::endl;
 	std::cout << "min2:"; dump4(dump_min2); std::cout << std::endl;
 	std::cout << "min3:"; dump4(dump_min3); std::cout << std::endl;
-	std::cout << "max :"; dump4(dump_max);  std::cout << std::endl;
-	std::cout << "zero:"; dump4(dump_zero); std::cout << std::endl;
 
     return 0;
 }
